2ad946951d63276926a74d20abaa1e34e99e0c57
mawaare82-cyber <mawaare82@gmail.com>
2026-06-08 16:02:49 +0300

    Remove TypeScript type annotation from children prop

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "../globals.css";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
